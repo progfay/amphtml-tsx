2be90a0dd06f9ea8aa9466849af392eae7378759
progfay <shun1856shun@gmail.com>
2019-11-20 00:10:17 +0900

    feat: generate amp tag name types

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,10 @@ const fetch = require('node-fetch')
 const main = async () => {
   const response = await fetch('https://playground.amp.dev/amphtml-hint.json')
   const { amp } = await response.json()
-  console.log(Object.keys(amp))
+  const AmpElementTagList = Object.keys(amp)
+    .filter(tag => tag.toLowerCase().startsWith('amp-'))
+    .map(tag => `'${tag.toLowerCase()}'`)
+  console.log(`export type AmpElementTagType = ${AmpElementTagList.join(' | ')}`)
 }
 
 main()
